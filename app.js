38d89553b1cee48c61943303efda4efef67fd1d3
Hugo Minzoni <50162408+hugominzoni@users.noreply.github.com>
2023-11-01 14:37:54 -0400

    add mobile css, finish js

--- app.js
+++ app.js
@@ -14,20 +14,23 @@ fetch('./data.json')
 	.then(res => res.json())
 	.then(data => {
 		data.forEach((element)=>{
-            console.log(element);
-            statusPage += `
-            <div class="sections">
-            <div class="sec reaction">
-              <div class="sec-left">
-                <img src="${element.icon}" alt="" />
-                <p>${element.category}</p>
-              </div>
-              <div class="nums"><span class="num">${element.score}</span> / 100</div>
-            </div>
+      const category = element.category;
+      const icon = element.icon;
+      const score = element.score;
+      statusPage += `
+        <div class="sections">
+        <div class="sec ${category.toLowerCase()}">
+          <div class="sec-left">
+            <img src="${icon}" alt="" />
+            <p>${category}</p>
           </div>
-            `
+          <div class="nums"><span class="num">${score}</span> / 100</div>
+        </div>
+        </div>
+      `
         })
         sectionRight.innerHTML = statusPage
+
 	})
 	.catch(err => {
         console.log(`error ${err}`)
